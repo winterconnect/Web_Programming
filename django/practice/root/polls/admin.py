91c7b98eec11235754cb6a1b531156aa7c116f2b
Winter Song <songjihyeon@hotmail.com>
2021-02-03 23:06:52 +0900

    making website polls and books

---
+++ django/practice/root/polls/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+from django.contrib import admin
+from polls.models import Question, Choice
+
+    
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline] # Choice 모델 클래스 같이보기
+    list_display = ('question_text', 'pub_date') # 레코드 리스트 컬럼 지정
+    list_filter = ['pub_date'] # 필터 사이드바 추가
+    search_fields = ['question_text'] # 검색박스 추가 
+    
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
